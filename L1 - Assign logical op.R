20425064141647869b4425b67dc1a412f5f5164b
Suman Bandyopadhyay <suman_1995@hotmail.com>
2025-12-29 09:05:51 +0530

    Add files via upload
    Test R file

---
+++ L1 - Assign logical op.R
@@ -0,0 +1,43 @@
+## Assignment
+
+a <- 8;
+b = 2;
+
+## arithmetic operations
+
+print(a+b); 
+print(a-b);
+
+c = a/b;
+print(c);
+
+d= a^3;
+d
+
+e = a%%3;
+e;
+
+f = a%/%b
+f
+
+## relational operator
+
+a==b;
+a!=b;
+
+x <- c(1:10)
+x>8;
+
+##logical operator
+
+c(TRUE,TRUE,FALSE) & c(TRUE,FALSE,FALSE);
+
+c(TRUE,TRUE,FALSE) && c(TRUE,FALSE,FALSE);
+
+c(TRUE,TRUE,FALSE) | c(TRUE,FALSE,FALSE);
+
+c(TRUE,TRUE,FALSE) || c(TRUE,FALSE,FALSE);
+
+
+
+
